6efc975ba5c12ee7a4894c6a2690ca992e079506
Himjune <himjune@mail.ru>
2018-08-21 18:47:34 +0300

    Try wh again

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.get('*', (req, res) => {
     res.sendFile('public/index.html', {root: '.'});
 });
 
-const port = process.env.PORT || 81;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
     console.log(`Server listening on port now: ${port}`);
 });
